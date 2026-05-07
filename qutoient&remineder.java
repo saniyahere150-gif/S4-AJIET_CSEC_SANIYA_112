96cde10359edbf84067cc4428e3f983024d1496c
saniyahere150-gif <saniyahere150@gmail.com>
2026-05-07 11:58:12 +0530

    Create qutoient&remineder.java

---
+++ qutoient&remineder.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        int x=20,b=4,q,r;
+        q=x/b;
+        r=x%b;
+        System.out.println(r);
+        System.out.println(q);
+    }
+}
