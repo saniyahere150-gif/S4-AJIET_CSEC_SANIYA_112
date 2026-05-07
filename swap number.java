f3fcf7b3818b33fcc3ee9b6b216e9945866ffe41
saniyahere150-gif <saniyahere150@gmail.com>
2026-05-07 11:58:56 +0530

    Create swap number.java

---
+++ swap number.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+         int x=20,b=4,c;
+         c=x;
+         x=b;
+         b=c;
+        System.out.println(x);
+        System.out.println(b);
+
+    }
+}
